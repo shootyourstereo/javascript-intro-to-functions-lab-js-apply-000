45a99d6f90f2742163db9642cb88c6980ecdd45e
Victoria <vikkanazarova@gmail.com>
2018-05-22 13:15:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  string.toLowerCase() === string
-  return "I can't hear you!";
-  string.toUpperCase() === string
-  return "YES INDEED!";
-  string === "I love you, Grandma."
-  return "I love you, too.";
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!";
+  } else if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
 }
