bb72ae15a540aace93dc5036b574f246b7dfbca4
Victoria <vikkanazarova@gmail.com>
2018-05-22 12:32:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,14 +22,3 @@ function sayHiToGrandma(string) {
   string === "I love you, Grandma."
   return "I love you, too."
 }
-}
-
-// function sayHiToGrandma(string) {
-//   string.toUpperCase() === string
-//   return "YES INDEED!"
-// }
-
-// function sayHiToGrandma(string) {
-//   string === "I love you, Grandma."
-//   return "I love you, too."
-// }
